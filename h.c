1a7cc1ce20c01885dad3c7349b47dd1a7a437dd5
yuwei5 <yuwei5@staff.sina.com.cn>
2014-01-10 19:04:59 +0800

    mvwin OK

--- h.c
+++ h.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+
 #include <termios.h>
 #include <term.h>
 #include <curses.h>
@@ -24,9 +26,19 @@ typedef struct _Locate
     int h;
 } Locate;
 
+typedef struct _Scale
+{
+    float x;
+    float y;
+    float w;
+    float h;
+} Scale;
+
+
 typedef struct _Win
 {
     Locate locate;
+    Scale scale;
     char name[64];
     char title[64];
     WINDOW *win;
@@ -35,7 +47,6 @@ typedef struct _Win
 typedef std::map<std::string, Win> Windows;
 Windows frames; // all frames
 Windows windows; // all content windows
-
 
 
 void init_keyboard()
@@ -52,6 +63,11 @@ void init_keyboard()
 void close_keyboard()
 {
     tcsetattr(0, TCSANOW, &initial_settings);
+}
+void cleanup()
+{
+    endwin();
+    system("stty sane");
 }
 
 int kbhit()
@@ -104,22 +120,52 @@ int form_back(WINDOW *win)
 
 int resize_allwin()
 {
+    Win *win = NULL;
     Windows::iterator iter;
     struct winsize size;
-    ioctl(STDIN_FILENO,TIOCGWINSZ,&size);
-    printf("%d\n",size.ws_col);
-    printf("%d\n",size.ws_row);
+    char msg[64] = {0};
+    int ret = 0;
 
+    ioctl(STDIN_FILENO,TIOCGWINSZ,&size);
 
     for (iter=windows.begin(); iter!=windows.end(); iter++) {
-        if (iter->first.compare("L1") == 0) {
-            iter->second.locate.w = size.ws_col * 0.8;
-        } else {
-            iter->second.locate.w = size.ws_col * 0.2;
+        win = &iter->second;
+        win->locate.x = size.ws_col * win->scale.x + 1;
+        win->locate.y = size.ws_row * win->scale.y + 1;
+        win->locate.w = size.ws_col * win->scale.w - 2;
+        win->locate.h = size.ws_row * win->scale.h - 2;
+
+        if (iter->first.compare("RCMD") == 0) {
+            win->locate.h = 4;
         }
-        iter->second.locate.h = size.ws_row - 1;
-        box(iter->second.win, '|', '-');
-        wrefresh(iter->second.win);
+
+        ret = wresize(win->win, win->locate.h, win->locate.w);
+        if (ERR == ret) {
+            printf("wresize failed:%s %d,%d\n", win->name, win->locate.x, win->locate.y);
+        }
+        ret = mvwin(win->win, win->locate.y, win->locate.x);
+        if (ERR == ret) {
+            cleanup();
+            printf("SCREEN=%d x %d\n", COLS, LINES);
+            printf("%d\n", win->win);
+            printf("mvwin failed:%s %d,%d\n", win->name, win->locate.x, win->locate.y);
+            printf("\t %d x %d\n", win->locate.w, win->locate.h);
+            exit(0);
+        }
+
+        //echo();
+        //touchwin(win->win);
+        //attron(A_REVERSE);
+        box(win->win, '|', '-');
+        //mvaddstr(2, 2,win->name);
+        mvwaddstr(win->win, 1, 1, win->name);
+        sprintf(msg, "%d,%d", win->locate.y, win->locate.x);
+        mvwaddstr(win->win, 2, 1, msg);
+        //attroff(A_REVERSE);
+        //move(1, 1);
+        //waddstr(win->win, win->name);
+        wrefresh(win->win);
+  //      }
     }
 }
 
@@ -129,10 +175,10 @@ int init_allwin()
     int i = 0;
     Win win[4] = {
             // x,y,width,height,name,title,WINDOW*
-            {{0, 0, 60, 60}, "L1", NULL, NULL}, 
-            {{100, 0, 20, 20}, "R1", NULL, NULL}, 
-            {{100, 10, 20, 20}, "R2", NULL, NULL}, 
-            {{100, 15, 20, 20}, "R3", NULL, NULL}, 
+            {{0, 0, 60, 60}, {0, 0, 0.8, 1},  "L1", NULL, NULL}, 
+            {{100, 0, 20, 20}, {0.8, 0, 0.2, 0.4}, "R1", NULL, NULL}, 
+            {{100, 10, 20, 20}, {0.8, 0.4, 0.2, 0.4}, "R2", NULL, NULL}, 
+            {{100, 15, 20, 20}, {0.8, 0.8, 0.2, 0.2}, "RCMD", NULL, NULL}, 
             };
 
 
@@ -144,12 +190,15 @@ int init_allwin()
             break;
         }
         scrollok(win[i].win, TRUE);
-        box(win[i].win, '|', '-');
-        wrefresh(win[i].win);
         windows.insert(Windows::value_type(win[i].name, win[i]));
     }
  
     return ret;
+}
+
+static void sig_winch(int signo)
+{
+    resize_allwin();
 }
 
 int main(int argc, char *argv[])
@@ -161,13 +210,14 @@ int main(int argc, char *argv[])
     int x = 0;
     int key = 0;
     int ch = 0;
-    WINDOW *win = NULL;
 
     init_keyboard();
     initscr();
 
     init_allwin();
     resize_allwin();
+
+    signal(SIGWINCH, sig_winch);
 
     /*
     // form_back(win);
@@ -183,25 +233,34 @@ aaa:
         usleep(500);
         if(kbhit()) {
             ch = readch();
-            wprintw(win, ".");
-            wrefresh(win);
+            // wprintw(win, ".");
+            // wrefresh(win);
         }
-        wprintw(win, ".");
-        wrefresh(win);
+        // wprintw(win, ".");
+        // wrefresh(win);
     }
 
 quit:
     close_keyboard();
-
-    endwin();
+    cleanup();
 
     for (int i = 30; i<129;i++) {
         printf("%d %c\t", i, i);
     }
     printf("\n");
 
-
-    system("stty sane");
+    struct winsize size;
+    Win *win = NULL;
+    ioctl(STDIN_FILENO,TIOCGWINSZ,&size);
+    Windows::iterator iter;
+    for (iter=windows.begin(); iter!=windows.end(); iter++) {
+        win = &iter->second;
+        printf("%s @(%d,%d), (%dx%d)\n", win->name,
+                win->locate.x, win->locate.y,
+                win->locate.w, win->locate.h,
+                win->title);
+    }
+    printf("SCREEN: %d x %d\n", size.ws_col, size.ws_row);
 
     return ret;
 }
--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-c++ h.c -g -O0 -lncurses -I/usr/include/ncurses -o h
+c++ h.c -g -O0 -lncurses -I/usr/include/ncurses -o dot
