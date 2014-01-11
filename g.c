7e7ecce55512d4ec2f85fc657377ca53e4545a2c
peng <yuwei5@staff.sina.com.cn>
2014-01-11 13:46:14 +0800

    top10+slowshow/game first OK

--- g.c
+++ g.c
@@ -26,30 +26,51 @@ typedef struct _Object
     int life;
     char face;
     int score;
+    int xstep;
+    int ystep;
 } Object;
 typedef std::vector<Object> Monsters;
 Monsters monsters;
 Object me;
+
+typedef std::vector<Object> Fires;
+Fires fires;
 
 static int rnd(int from, int to)
 {
     return (rand() % (to - from + 1)) + from;
 }
 
+static void draw_me()
+{
+    mvwaddch(win_disp->win, me.y, me.x, me.face);
+}
+
+static void draw_refresh()
+{
+    wrefresh(win_disp->win);
+}
+
 static void draw()
 {
+    unsigned i = 0;
+
     if (NULL == win_disp) {
         return;
     }
     wclear(win_disp->win);
     char msg[16] = {0};
-    sprintf(msg, "Level %d Round:%d", level, round);
+    sprintf(msg, "L%d R%d Score:%d", level, round, me.score);
     mvwaddstr(win_disp->win, 0, 1, msg);
 
     mvwaddch(win_disp->win, me.y, me.x, me.face);
-    for (unsigned i=0; i<monsters.size(); i++) {
+    for (i=0; i<monsters.size(); i++) {
         Object *m = &monsters[i];
         mvwaddch(win_disp->win, m->y, m->x, m->face);
+    }
+    for (i=0; i<fires.size(); i++) {
+        Object *f = &fires[i];
+        mvwaddch(win_disp->win, f->y, f->x, f->face);
     }
     
     wrefresh(win_disp->win);
@@ -67,17 +88,64 @@ static void init_game(int level)
         m.y = rnd(1, 10);
         m.life = 1;
         m.face = '@';
+        m.score = 10;
         monsters.push_back(Monsters::value_type(m));
     }
 }
 
-static void monsters_round()
+static int fire_fly()
 {
+    int hit = 0;
+    int ret = 0;
+
+    for (unsigned i=0; i<fires.size(); i++) {
+        ret++;
+        hit = 0;
+        Object *f = &fires[i];
+        mvwaddch(win_disp->win, f->y, f->x, ' '); // erase old face
+        f->x += f->xstep;
+        f->y += f->ystep;
+        if (f->x < 0 || f->x >= win_disp->locate.w
+                || f->y < 0 || f->y >= win_disp->locate.h) {
+            fires.erase(fires.begin() + i);
+            break;
+        }
+        // hit test
+        for (unsigned j=0; j<monsters.size(); j++) {
+            Object *m = &monsters[j];
+            if (f->x == m->x && f->y == m->y) {
+                m->life--;
+                if (m->life <= 0) {
+                    hit = 1;
+                    monsters.erase(monsters.begin() + j);
+                    me.score += m->score;
+                    break;
+                }
+            }
+        }
+        if (hit) {
+            continue;
+        }
+        mvwaddch(win_disp->win, f->y, f->x, f->face);
+        draw_me();
+        wrefresh(win_disp->win);
+
+        usleep(1000*100);
+    }
+
+    return ret;
+}
+
+static int monsters_round()
+{
+    int ret = 0;
     int yoffset = 0;
     int xoffset = 0;
 
     for (unsigned i=0; i<monsters.size(); i++) {
+        ret++;
         Object *m = &monsters[i];
+        mvwaddch(win_disp->win, m->y, m->x, ' ');
         yoffset = me.y - m->y;
         xoffset = me.x - m->x;
         if (abs(yoffset) > abs(xoffset)) {
@@ -93,17 +161,61 @@ static void monsters_round()
                 m->x--;
             }
         }
+        if (m->y == me.y && m->x == me.x) {
+            me.life--;
+        }
         mvwaddch(win_disp->win, m->y, m->x, m->face);
-        draw();
+        wrefresh(win_disp->win);
+        usleep(1000*200);
     }
     
+    return ret;
 }
 
-static void do_round(char *key)
+static void gameover()
 {
+    int pos = 1;
     wclear(win_disp->win);
+    draw();
+    mvwaddstr(win_disp->win, pos++, 1, "Game Over!");
+    mvwaddstr(win_disp->win, pos++, 1, "No.  name   score ");
+    mvwaddstr(win_disp->win, pos++, 1, "------------------");
+    mvwaddstr(win_disp->win, pos++, 1 ,"1.   0xFF      160");
+    mvwaddstr(win_disp->win, pos++, 1 ,"2.   0xFF      120");
+    mvwaddstr(win_disp->win, pos++, 1 ,"3.   ....    .....");
+    mvwaddstr(win_disp->win, pos++, 1 ,"4.   ....    .....");
+    mvwaddstr(win_disp->win, pos++, 1 ,"5.   ....    .....");
+    mvwaddstr(win_disp->win, pos++, 1 ,"6.   ....    .....");
+    wrefresh(win_disp->win);
 
-    for (unsigned i=0; i<strlen(key); i++) {
+    readch();
+    return;
+}
+
+static void new_fire(int xstep, int ystep)
+{
+    Object f;
+    f.x = me.x;
+    f.y = me.y;
+    f.face = '.';
+    f.xstep = xstep;
+    f.ystep = ystep;
+    fires.push_back(f);
+}
+
+static int do_round(char *key)
+{
+    unsigned i = 0;
+    int ret = 0;
+    int len = strlen(key);
+
+    for (i=0; i<monsters.size(); i++) {
+        ret++;
+        Object *m = &monsters[i];
+        m->score += len*10;
+    }
+
+    for (i=0; i<len; i++) {
         char cmd = key[i];
         switch(cmd) {
             case 'w':
@@ -117,34 +229,51 @@ static void do_round(char *key)
                 }
                 break;
             case 's':
-                if (me.y < 10) {
+                if (me.y < win_disp->locate.h) {
                     me.y++;
                 }
                 break;
             case 'd':
-                if (me.x < 10) {
+                if (me.x < win_disp->locate.w) {
                     me.x++;
                 }
                 break;
             case 'h':
+                new_fire(-1, 0);
                 break;
             case 'j':
+                new_fire(0, 1);
                 break;
             case 'k':
+                new_fire(0, -1);
                 break;
             case 'l':
+                new_fire(1, 0);
                 break;
             default:
                 break;
         }
+        ret = monsters_round();
         draw();
-        monsters_round();
-        usleep(300);
+        
+        fire_fly();
+        if (me.life <= 0) {
+            gameover();
+            return -1;
+        }
+
+        if (ret <= 0) {
+            while (fire_fly() > 0);
+            break;
+        }
+        usleep(1000*500);
     }
+    
+    return ret;
 }
 
 // return value: 0:running; not 0:quit;
-static int wait_user_cmd(char *keybuf)
+static int wait_user_cmd(char *keybuf, size_t buflen)
 {
     int ch = 0;
     int cursor = 0;
@@ -152,7 +281,7 @@ static int wait_user_cmd(char *keybuf)
     while(1) {
         ch = readch();
         keybuf[cursor++] = ch;
-        if (ch == '\n' || cursor >= (int)sizeof(keybuf) - 1) {
+        if (ch == '\n' || cursor >= buflen - 1) {
             wclear(win_input->win);
             wrefresh(win_input->win);
             return cursor;
@@ -175,6 +304,7 @@ static int wait_user_cmd(char *keybuf)
 
 void game_main_loop(Win *disp, Win *input)
 {
+    int ret = 0;
     static char keybuf[32] = {0};
 
     win_disp = disp;
@@ -187,13 +317,22 @@ void game_main_loop(Win *disp, Win *input)
     me.score = 0;
     round = 0;
     init_game(level);
+    wclear(win_disp->win);
+    draw();
 
     do {
         round++;
         memset(keybuf, 0, sizeof(keybuf));
-        draw();
-        wait_user_cmd(keybuf);
-        do_round(keybuf);
+        wait_user_cmd(keybuf, sizeof(keybuf));
+        ret = do_round(keybuf);
+        if (ret == -1) {
+            break;
+        }
+        if (ret == 0) {
+            round = 0;
+            init_game(++level);
+            continue;
+        }
         if (keybuf[0] == 'q') {
             break;
         }
